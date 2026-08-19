1f76dc2105bce5b57cf6d86ce4cf49cb7084f4ad
Simon Roder <simon.roder@adesso.de>
2026-08-19 19:58:28 +0200

    naming the apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,6 +40,16 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+
+    applicationVariants.all {
+        val variant = this
+        variant.outputs
+            .map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+            .forEach { output ->
+                output.outputFileName =
+                    "apex_push-${variant.versionName}-${variant.buildType.name}.apk"
+            }
+    }
 }
 
 flutter {
